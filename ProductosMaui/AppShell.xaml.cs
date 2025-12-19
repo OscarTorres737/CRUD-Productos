9977559dc7c2a75212fb22a7124141cec3d3ad53
Oscar Ariel Torres Martinez <oscar.torresmartinez@outlook.com>
2025-12-19 13:30:49 -0600

    Agregar archivos de proyecto.

---
+++ ProductosMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductosMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ProductosMaui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ProductosMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
